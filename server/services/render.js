1783250051e0127353a04032ca13be0cea75fdac
Khoirul Muttaqin <khoirul.stark@gmail.com>
2022-07-17 11:09:19 +0700

    init5

--- server/services/render.js
+++ server/services/render.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 
 exports.homeRoutes = (req, res)=>{
-    axios.get('http://localhost:3000/api/users')
+    axios.get('https://nodejs-backend-server-app.herokuapp.com/api/users')
         .then(function(responses){
             res.render('index', {users : responses.data});
         })
