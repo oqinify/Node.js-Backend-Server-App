1149ad4ae4a008968780b7eaf6723b70c03f3acf
Oqinify <khoirul.stark@gmail.com>
2022-07-17 22:53:33 +0700

    Change host to online

--- server/services/render.js
+++ server/services/render.js
@@ -16,7 +16,7 @@ exports.add_user = (req, res)=>{
 }
 
 exports.update_user = (req, res)=>{
-    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
+    axios.get('https://nodejs-backend-server-app.herokuapp.com/api/users', { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user",{ user : userdata.data})
         })
