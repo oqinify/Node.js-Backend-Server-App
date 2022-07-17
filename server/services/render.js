ab6b868fe7b420ded5b88d3fca694bdf54515002
Oqinify <khoirul.stark@gmail.com>
2022-07-17 22:40:46 +0700

    Test

--- server/services/render.js
+++ server/services/render.js
@@ -2,13 +2,14 @@ const axios = require('axios');
 
 
 exports.homeRoutes = (req, res)=>{
-    axios.get('http://localhost:3000/api/users')
-        .then(function(response){
-            res.render('index', {users : response.data});
-        })
-        .catch(err => {
-            res.send(err)
-        })
+    // axios.get('http://localhost:3000/api/users')
+    //     .then(function(response){
+    //         res.render('index', {users : response.data});
+    //     })
+    //     .catch(err => {
+    //         res.send(err)
+    //     })
+        res.render('index');
 }
 
 exports.add_user = (req, res)=>{
