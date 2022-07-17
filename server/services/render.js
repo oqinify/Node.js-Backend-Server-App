1af3f1b26bbe6932a6843f3e69dd0d3126128beb
Oqinify <khoirul.stark@gmail.com>
2022-07-17 22:48:07 +0700

    Change host

--- server/services/render.js
+++ server/services/render.js
@@ -2,14 +2,13 @@ const axios = require('axios');
 
 
 exports.homeRoutes = (req, res)=>{
-    // axios.get('http://localhost:3000/api/users')
-    //     .then(function(response){
-    //         res.render('index', {users : response.data});
-    //     })
-    //     .catch(err => {
-    //         res.send(err)
-    //     })
-        res.render('index');
+    axios.get('https://nodejs-backend-server-app.herokuapp.com/api/users')
+        .then(function(response){
+            res.render('index', {users : response.data});
+        })
+        .catch(err => {
+            res.send(err)
+        })
 }
 
 exports.add_user = (req, res)=>{
